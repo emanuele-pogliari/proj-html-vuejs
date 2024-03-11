ba5c1c249e1e903512ede6348c2a62c01f29b295
emanuele-pogliari <manu.pogliari@gmail.com>
2024-03-11 22:37:56 +0100

    highlighted section filled with static data

--- src/components/AppHighlighted.vue
+++ src/components/AppHighlighted.vue
@@ -5,14 +5,28 @@ export default{
 </script>
 
 <template> 
-<div class="container" id="highlighted">
+<div class="container-fluid p-0" id="highlighted">
+    <div class="img-section position-relative ">
+        <img src="/public/images/banner14-2x.jpg" class="img-fluid" alt="">
+        <div class="position-absolute top-50 start-50 text-white translate-middle-x text-center">
+            <div>play</div>
+            <div class="fs-1">Crossifit Workout of the day</div>
+            <div>November 15th, 2019</div>
+            <a href="">View all videos</a>
+        </div>
+    </div>
 </div> 
      
 </template>
 
 <style lang="scss">
 #highlighted{
-    background-color: honeydew;
     height: 640px;
+    
+    .img-section{
+        width: 100%;
+        height: 100%;
+        filter: linear-gradient(151deg, #4154ff 35%, #ff414b 100%); 
+    }
 }
 </style>
