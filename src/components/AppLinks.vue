449376dada6f1fba09d0cf91e11aa7b3efef3d6c
emanuele-pogliari <manu.pogliari@gmail.com>
2024-03-11 22:16:54 +0100

    links section filled with static data

--- src/components/AppLinks.vue
+++ src/components/AppLinks.vue
@@ -7,6 +7,49 @@ export default{
 <template> 
 <div class="container" id="links">
 
+    <div class="row">
+        <div class="col-3">
+            <img src="/public/images/sponsor-1-2x.png" alt="">
+            <div class="">
+                <i class="fa-solid fa-chevron-up"></i>
+                <i class="fa-solid fa-chevron-up"></i>
+                <i class="fa-solid fa-chevron-up"></i>
+            </div>
+            <p>Tristique aliquam in nullam habitasse nunc, amet vel consectetur. Nulla in suspendisse dolor, aliquet.</p>
+            <a href="">Visit Yoga Studio</a>
+            
+        </div>
+        <div class="col-3">
+            <img src="/public/images/sponsor-2-2x.png" alt="">
+            <div class="">
+                <i class="fa-solid fa-chevron-up"></i>
+                <i class="fa-solid fa-chevron-up"></i>
+                <i class="fa-solid fa-chevron-up"></i>
+            </div>
+            <p>Feugiat etiam dui mauris pharetra mauris sed pharetra. Convallis sapien ornare cras faucibus nulla porta dui a.</p>
+            <a href="">Join Fitness Center</a>
+        </div>
+        <div class="col-3">
+            <img src="/public/images/sponsor-3-2x.png" alt="">
+            <div class="">
+                <i class="fa-solid fa-chevron-up"></i>
+                <i class="fa-solid fa-chevron-up"></i>
+                <i class="fa-solid fa-chevron-up"></i>
+            </div>
+            <p>Nisl at quam adipiscing montes, nec, adipiscing commodo aliquam tincidunt. Ligula in habitant et sagittis imperdiet.</p>
+            <a href="">Join Fitness Center</a>
+        </div>
+        <div class="col-3">
+            <img src="/public/images/sponsor-4-2x.png" alt="">
+            <div class="">
+                <i class="fa-solid fa-chevron-up"></i>
+                <i class="fa-solid fa-chevron-up"></i>
+                <i class="fa-solid fa-chevron-up"></i>
+            </div>
+            <p>Proin in viverra maecenas et mattis ut nibh enim. Gravida ultricies mi porta lacus. Et posuere velit sed nec eleifend.</p>
+            <a href="">Visit Power Gym</a>
+        </div>
+    </div>
 
 </div> 
      
@@ -14,7 +57,6 @@ export default{
 
 <style lang="scss">
 #links{
-    background-color: crimson;
     height: 555px;
 }
 </style>
