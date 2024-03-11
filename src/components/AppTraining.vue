65a810b2b255d9e43136cbe898a5417d6b5be21c
emanuele-pogliari <manu.pogliari@gmail.com>
2024-03-11 21:50:21 +0100

    training section filled with static data

--- src/components/AppTraining.vue
+++ src/components/AppTraining.vue
@@ -6,6 +6,29 @@ export default{
 
 <template> 
 <div class="container" id="training">
+    <div class="row p-5">
+        <div class="col-4 border border-2">
+            <div>icona</div>
+            <p>Team training</p>
+            <small>Find a partener</small>
+            <p>Vestibulum, curabitur eu sem nibh ultrices sit nulla adipiscing. Nisl sit fames amet senectus eget sed duis vehicula. Tristique.</p>
+            <a href="">Find a partner</a>
+        </div>
+        <div class="col-4 border border-2">
+            <div>icona</div>
+            <p>Crossifit workout</p>
+            <small>Push your limits</small>
+            <p>Vestibulum, curabitur eu sem nibh ultrices sit nulla adipiscing. Nisl sit fames amet senectus eget sed duis vehicula. Tristique.</p>
+            <a href="">Learn about crossfit</a> 
+        </div>
+        <div class="col-4 border border-2">
+            <div>icona</div>
+            <p>Weight loss plan</p>
+            <small>Shed those pounds</small>
+            <p>At laoreet senectus volutpat diam vel sed sed amet pellentesque. Lobortis ut aliquam risus purus. A tellus ut etiam.</p>
+            <a href="">Schedule a workout</a> 
+        </div>
+    </div>
 
 
 </div> 
