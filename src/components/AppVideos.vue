ee95f8fdaad49b55340f38754c6d2caf7d28de33
emanuele-pogliari <manu.pogliari@gmail.com>
2024-03-11 21:13:55 +0100

    video section filled with static data

--- src/components/AppVideos.vue
+++ src/components/AppVideos.vue
@@ -6,15 +6,51 @@ export default{
 
 <template> 
 <div class="container" id="videos">
-
+    <div class="row">
+        <div class="col d-flex justify-content-between">
+            <div>Featured Playlist</div>
+            <div>View all videos</div>
+        </div>
+        <div class="video-container d-flex">
+            <div class="col-4 px-4 single-video">
+                <img class="img-fluid" src="/public/images/video2-2x.jpg" alt="">
+                <div>
+                    <p>Thighs & glute workout</p>
+                    <small>Increase your mobility</small>
+                </div>
+            </div>
+            <div class="col-4 px-4 single-video">
+                <img class="img-fluid" src="/public/images/video7-2x.jpg" alt="">
+                <div>
+                    <p>Lift, firm & perk up</p>
+                    <small>Feel young again</small>
+                </div>
+            </div>
+            <div class="col-4 px-4 single-video">
+                <img class="img-fluid" src="/public/images/video9-2x.jpg" alt="">
+                <div>
+                    <p>Slim & trim your waist</p>
+                    <small>Increase your mobility</small>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </div> 
      
 </template>
 
-<style lang="scss">
+<style scoped lang="scss">
 #videos{
     background-color: antiquewhite;
     height: 705px;
+
+    .single-video{
+        img{
+        border-radius: 4px;
+        border-bottom-right-radius: 40px;
+        overflow: hidden;
+        }
+    }
 }
 </style>
